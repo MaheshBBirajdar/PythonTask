ed7bb5403010bf8243862f227824210983ce8948
Mahesh Birajdar <98763822+MaheshBBirajdar@users.noreply.github.com>
2023-06-05 00:12:09 +0530

    Update urls.py

--- upforce/task/urls.py
+++ upforce/task/urls.py
@@ -4,23 +4,23 @@ from .views import *
 
 urlpatterns = [
 
-    path('user/list', UserListView.as_view(), name='userlist' ),
-    path('user/create/', UserCreateView.as_view(),name='usercreate' ),
-    path('user/<int:pk>/', UserRetrieveView.as_view(),name='userR' ),
-    path('user/update/<int:pk>/', UserUpdateView.as_view(),name='userU' ),
-    path('user/delete/<int:pk>/', UserDestroyView.as_view(),name='userD' ),
+    path('user/list', UserListView.as_view()),
+    path('user/create/', UserCreateView.as_view()),
+    path('user/<int:pk>/', UserRetrieveView.as_view() ),
+    path('user/update/<int:pk>/', UserUpdateView.as_view()),
+    path('user/delete/<int:pk>/', UserDestroyView.as_view() ),
 
-    path('post/list', PostListView.as_view(), name='Postlist' ),
-    path('post/create/', PostCreateView.as_view(),name='Postcreate' ),
-    path('post/<int:pk>/', PostRetrieveView.as_view(),name='PostR' ),
-    path('post/update/<int:pk>/', PostUpdateView.as_view(),name='PostU' ),
-    path('post/delete/<int:pk>/', PostDestroyView.as_view(),name='PostD' ),
+    path('post/list', PostListView.as_view() ),
+    path('post/create/', PostCreateView.as_view() ),
+    path('post/<int:pk>/', PostRetrieveView.as_view() ),
+    path('post/update/<int:pk>/', PostUpdateView.as_view() ),
+    path('post/delete/<int:pk>/', PostDestroyView.as_view() ),
 
-    path('like/list', LikeListView.as_view(), name='Likelist' ),
-    path('like/create/', LikeCreateView.as_view(),name='Likecreate' ),
-    path('like/<int:pk>/', LikeRetrieveView.as_view(),name='LikeR' ),
-    path('like/update/<int:pk>/', LikeUpdateView.as_view(),name='LikeU' ),
-    path('like/delete/<int:pk>/', LikeDestroyView.as_view(),name='LikeD' ),
+    path('like/list', LikeListView.as_view() ),
+    path('like/create/', LikeCreateView.as_view() ),
+    path('like/<int:pk>/', LikeRetrieveView.as_view()),
+    path('like/update/<int:pk>/', LikeUpdateView.as_view()),
+    path('like/delete/<int:pk>/', LikeDestroyView.as_view() ),
 
 
 
